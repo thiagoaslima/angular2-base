e77d765da7cb0b938f80746e869ad339d77d034a
Thiago Lima <thiagoaslima@gmail.com>
2016-07-27 14:44:28 -0300

    Update systemjs.config.js
    entry point corrected to main.js

--- systemjs.config.js
+++ systemjs.config.js
@@ -9,7 +9,7 @@
 
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
-        'app':                        { main: 'boot.js',  defaultExtension: 'js' },
+        'app':                        { main: 'main.js',  defaultExtension: 'js' },
         'rxjs':                       { defaultExtension: 'js' },
     };
 
